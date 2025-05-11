222fbd0e35a755ad56875f45dfc7263a5c091dd1
Vivek Kr Sharma <vivekshivam0987@gmail.com>
2025-05-11 08:48:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.set(".menu", {
+    display: "none",
+})
+function openMenu() {
+    gsap.to(".menu", {
+        display: "block",
+        scale: 1,
+        duration: 0.5,
+        ease: "power2.out",
+    });
+}
+
+function closeMenu() {
+    gsap.to(".menu", {
+        display: "none",
+        scale: 0.9,
+        duration: 0.2,
+        ease: "power2.in",
+        onComplete: () => {
+            gsap.set(".menu", { display: "none" });
+        }
+    });
+}
