c90ce11af07c4877d27712fbf12c86b868beada5
Vivek Kr Sharma <vivekshivam0987@gmail.com>
2025-05-13 08:31:07 -0700

    veronique2.0

--- script.js
+++ script.js
@@ -25,3 +25,52 @@ function closeMenu() {
         },
     });
 }
+
+gsap.set(".sameIMG", {
+    opacity: 0,
+});
+
+function imgAni() {
+    document.querySelectorAll(".sameIMG").forEach(function (img) {
+        img.addEventListener("mouseover", function () {
+            gsap.to(img, {
+                opacity: 1,
+                duration: 0.5,
+                ease: "power2.out",
+            });
+        });
+        img.addEventListener("mouseout", function () {
+            gsap.to(img, {
+                opacity: 0,
+                duration: 0.5,
+                ease: "power2.in",
+            });
+        });
+    });
+}
+imgAni();
+
+// Mouse follower on hover
+const follower = document.querySelector(".cursor");
+
+const targetDiv = document.querySelector(".project"); // Add this class to your target div
+
+
+    targetDiv.addEventListener("mouseenter", () => {
+        follower.style.transform = "translate(-50%, -50%) scale(1)";
+        follower.style.display = "block";
+        follower.style.duration = "0.3s";   
+    });
+    targetDiv.addEventListener("mouseleave", () => {
+        follower.style.transform = "translate(-50%, -50%) scale(0)";
+        follower.style.display = "none";
+        follower.style.duration = "0.3s";
+    });
+    targetDiv.addEventListener("mousemove", (e) => {
+        gsap.to(follower, {
+            left: e.pageX,
+            top: e.pageY,
+            duration: 0.2,
+            ease: "power2.out"
+        });
+    });
